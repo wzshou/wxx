85293c5eedc0a57d65de477992f70a0aaa5e985f
wzs <1224708605@qq.com>
2017-05-13 12:02:02 +0800

    第一次

---
+++ Example/Pods/Target Support Files/wxx/wxx-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_wxx : NSObject
+@end
+@implementation PodsDummy_wxx
+@end
---
+++ wxx/Classes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cocoapods,5-8
+//
+//  Created by Wzs 王 on 2017/5/8.
+//  Copyright © 2017年 Wzs 王. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
